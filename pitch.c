c7dd5136a6a73b7fe6da3cee8baf727ccefc6fa6
Edro Gonzales <edrogonzales@gmail.com>
2024-03-30 16:40:40 -0700

    edit functionality of pitch.c

--- pitch.c
+++ pitch.c
@@ -27,22 +27,20 @@ int checkFormat(WAVHEADER header) {
 /**
  * Format file name output.
  *
- * @param fileName to be formatted
- * @param length of the current file name
  * @param newFileName is the pointer to the new file name
+ * @param fileName to be formatted
  * @return newFileName formatted with copy prepended
  */
-void formatFileName(char *newFileName, char *fileName) {
+void formatFileName(char *newFileName, const char *fileName) {
     if (newFileName == NULL) {
         printf("Error: cannot allocate memory for new file name.\n");
         return;
     }
 
     strcpy(newFileName, "./output/");
-    unsigned int baseFileNameLength = strlen(fileName) - 4;
+    size_t baseFileNameLength = strlen(fileName) - 4;
     strncat(newFileName, fileName, baseFileNameLength);
     strcat(newFileName, "-pitched.wav");
-    printf("New file name: %s\n", newFileName);
 }
 
 /**
@@ -70,14 +68,17 @@ int main(int argc, char* argv[]) {
     char* fileWAV = strrchr(fileWAVPath, '/') + 1;
     if (!checkFormat(header)) {
         printf("Error, %s is not a .wav file\n", fileWAV);
+        fclose(inputAudio);
         return 1;
     }
 
+    fseek(inputAudio, sizeof(WAVHEADER), SEEK_SET);
+
     double pitchFactor = 1.2;
-    int newAudioSize = (int)(header.subchunk2Size / pitchFactor);
+    int newAudioSize = (int) (header.subchunk2Size / pitchFactor);
     int16_t* audioData = malloc(header.subchunk2Size);
-    int16_t* newAudioData = malloc(newAudioSize);
-    fread(audioData, header.subchunk2Size, 1, inputAudio);
+    int16_t* newAudioData = malloc(newAudioSize * sizeof(int16_t));
+    fread(audioData, sizeof(int16_t), header.subchunk2Size / sizeof(int16_t), inputAudio);
 
     for (int i = 0; i < newAudioSize; i++) {
         int sourceIndex = (int)(i * pitchFactor);
@@ -87,12 +88,18 @@ int main(int argc, char* argv[]) {
     char newFileName[MAX_FILE_NAME];
     formatFileName(newFileName, fileWAV);
 
-    // TODO: almost complete -- it will output a corrupted .wav file
     FILE* outputAudio = fopen(newFileName, "wb");
-    header.subchunk2Size = newAudioSize;
+    if (outputAudio == NULL) {
+        printf("Error creating output file\n");
+        fclose(inputAudio);
+        free(audioData);
+        free(newAudioData);
+        return 1;
+    }
 
+    header.subchunk2Size = newAudioSize * sizeof(int16_t);
     fwrite(&header, sizeof(WAVHEADER), 1, outputAudio);
-    fwrite(newAudioData, newAudioSize, 1, outputAudio);
+    fwrite(newAudioData, sizeof(int16_t), newAudioSize, outputAudio);
 
     fclose(inputAudio);
     fclose(outputAudio);
