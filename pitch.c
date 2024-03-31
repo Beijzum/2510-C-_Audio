ae2e55174831c79774b29dcf2f67068644dc8e56
Edro Gonzales <edrogonzales@hotmail.com>
2024-03-30 22:27:09 -0700

    Add buffer iteration to fix issues

--- pitch.c
+++ pitch.c
@@ -12,7 +12,6 @@
 #define MAX_FILE_NAME 50
 
 
-
 /**
  * Check if the file is a .wav file by checking the format field in the header
  *
@@ -24,6 +23,20 @@ int checkFormat(WAVHEADER header) {
            header.format[2] == 'V' && header.format[3] == 'E';
 }
 
+
+/**
+ * Check if the file is a .wav file by checking the format field in the header
+ *
+ * @param header the header of the .wav file
+ * @return stream block of bytes
+ */
+int getBlockSize(WAVHEADER header) {
+    int channels = header.numChannels;
+    int BytesPerSample = header.bitsPerSample / 8;
+    return channels * BytesPerSample;
+}
+
+
 /**
  * Format file name output.
  *
@@ -31,16 +44,18 @@ int checkFormat(WAVHEADER header) {
  * @param fileName to be formatted
  * @return newFileName formatted with copy prepended
  */
-void formatFileName(char *newFileName, const char *fileName) {
+char* formatFileName(const char *fileName) {
+    char* newFileName = (char*) malloc(MAX_FILE_NAME * sizeof(char));
     if (newFileName == NULL) {
         printf("Error: cannot allocate memory for new file name.\n");
-        return;
+        return NULL;
     }
 
     strcpy(newFileName, "./output/");
     size_t baseFileNameLength = strlen(fileName) - 4;
     strncat(newFileName, fileName, baseFileNameLength);
     strcat(newFileName, "-pitched.wav");
+    return newFileName;
 }
 
 /**
@@ -72,36 +87,40 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    double pitchFactor = 1.2;
-    int newAudioSize = (int) (header.subchunk2Size / pitchFactor);
-    int16_t* audioData = malloc(header.subchunk2Size);
-    int16_t* newAudioData = malloc(newAudioSize * sizeof(int16_t));
-    fread(audioData, sizeof(int16_t), header.subchunk2Size / sizeof(int16_t), inputAudio);
-
-    for (int i = 0; i < newAudioSize; i++) {
-        newAudioData[i] = audioData[i];
-    }
-
-    char newFileName[MAX_FILE_NAME];
-    formatFileName(newFileName, fileWAV);
-
-    FILE* outputAudio = fopen(newFileName, "wb");
-    if (outputAudio == NULL) {
-        printf("Error creating output file\n");
-        fclose(inputAudio);
-        free(audioData);
-        free(newAudioData);
-        return 1;
-    }
-
-    header.subchunk2Size = newAudioSize;
+    char* outputPath = formatFileName(fileWAV);
+    FILE* outputAudio = fopen(outputPath, "wb");
     fwrite(&header, sizeof(WAVHEADER), 1, outputAudio);
-    fwrite(newAudioData, sizeof(int16_t), newAudioSize, outputAudio);
+
+    WORD blockSize = getBlockSize(header);
+    WORD buffer[blockSize];
+    WORD maxSampleValue = 0;
+    while (fread(&buffer, blockSize, 1, inputAudio)) {
+        for (int i = 0; i < blockSize; i++) {
+            buffer[i] = (buffer[i] * 3) / 2;
+            if (buffer[i] > maxSampleValue) {
+                maxSampleValue = buffer[i];
+            }
+        }
+
+        double scalingFactor = 1.0;
+        if (header.bitsPerSample == 8) {
+            scalingFactor = 128.0 / maxSampleValue;
+        } else if (header.bitsPerSample == 16) {
+            scalingFactor = 32768.0 / maxSampleValue;
+        }
+        for (int i = 0; i < blockSize; i++) {
+            buffer[i] = (WORD)(buffer[i] * scalingFactor);
+        }
+
+        header.subchunk2Size = header.subchunk2Size * scalingFactor;
+        fseek(outputAudio, 0, SEEK_SET);
+        fwrite(&header, sizeof(WAVHEADER), 1, outputAudio);
+        fwrite(&buffer, blockSize, 1, outputAudio);
+    }
 
     fclose(inputAudio);
     fclose(outputAudio);
-    free(newAudioData);
-    free(audioData);
+    free(outputPath);
 
     return 0;
 }
